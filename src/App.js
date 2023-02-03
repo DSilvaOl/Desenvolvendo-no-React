0840778a8912354e2b80189cb62cb900fc06c1fc
Diego Silva Oliveira <dhyegool@hotmail.com>
2023-02-03 15:23:43 -0300

    novos arquivos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import "./App.css";
-import SayMyName from './components/SayMyName'
-import Pessoa from './components/Pessoa'
+import SayMyName from "./components/SayMyName";
+import Pessoa from "./components/Pessoa";
+import Frase from "./components/Frase";
+import List from "./components/List";
 
 function App() {
-
-const nome= 'Cláudia'
+  const nome = "Cláudia";
 
   return (
     <div className="App">
-    
-      <SayMyName nome='Diego'/>
-      <SayMyName nome='Kekel'/>
-      <SayMyName nome= {nome} />
-      <Pessoa nome= 'Calleb' 
-      idade='35' 
-      profissao= 'Programador'
-       foto='https://via.placeholder.com/150'
-
+      <h1>Testando CSS</h1>
+      <Frase />
+      <Frase />
+      <SayMyName nome="Diego" />
+      <SayMyName nome="Kekel" />
+      <SayMyName nome={nome} />
+      <Pessoa
+        nome="Calleb"
+        idade="35"
+        profissao="Programador"
+        foto="https://via.placeholder.com/150"
       />
-
+      <List />
     </div>
   );
 }
--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,8 +1,10 @@
-function Frase () {
-    return (
-        <div>
-            <p>Este é um componente com uma frase</p>
-        </div>
-    )
+import styles from "./Frase.module.css";
+
+function Frase() {
+  return (
+    <div className={styles.fraseContainer}>
+      <p className={styles.fraseContent}> Este é um componente com uma frase! </p>
+    </div>
+  );
 }
-export default Frase
+export default Frase;
