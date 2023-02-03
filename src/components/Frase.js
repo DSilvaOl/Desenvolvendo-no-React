a871e1ee5d6145cfbab645975e1cfdbe6dd9f0d7
Diego Silva Oliveira <dhyegool@hotmail.com>
2023-02-03 15:33:31 -0300

    atualização1

--- src/components/Frase.js
+++ src/components/Frase.js
@@ -3,7 +3,10 @@ import styles from "./Frase.module.css";
 function Frase() {
   return (
     <div className={styles.fraseContainer}>
-      <p className={styles.fraseContent}> Este é um componente com uma frase! </p>
+      <p className={styles.fraseContent}>
+        {" "}
+        Este é um componente com uma frase!{" "}
+      </p>
     </div>
   );
 }
