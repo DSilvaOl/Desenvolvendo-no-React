a77d5d8ce6b3555b7364ef202a57d15419b44fba
Diego Silva Oliveira <dhyegool@hotmail.com>
2023-02-07 22:26:55 -0300

    atualização2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import "./App.css";
-import SayMyName from "./components/SayMyName";
-import Pessoa from "./components/Pessoa";
-import Frase from "./components/Frase";
-import List from "./components/List";
+import Evento from "./components/Evento";
+import Form from "./components/Form";
+
 
 function App() {
-  const nome = "Cláudia";
 
   return (
     <div className="App">
-      <h1>Testando CSS</h1>
-      <Frase />
-      <Frase />
-      <SayMyName nome="Diego" />
-      <SayMyName nome="Kekel" />
-      <SayMyName nome={nome} />
-      <Pessoa
-        nome="Calleb"
-        idade="35"
-        profissao="Programador"
-        foto="https://via.placeholder.com/150"
-      />
-      <List />
-    </div>
+      <h1>Testando Eventos</h1>
+      <Evento numero= '1'/>
+      <Evento numero= '2' />
+      <Form />
+      </div>
   );
 }
 
