a4bd65546191bafc45758643f93effd15f2bd1b8
Diego Silva Oliveira <dhyegool@hotmail.com>
2023-04-12 21:16:44 -0300

    React Icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,23 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+import style from "./Footer.module.css";
+
 function Footer() {
-  return <footer>Rodapé</footer>;
+  return (
+    <footer>
+      <ul className={style.social_list}>
+        <li>
+          <FaFacebook />
+        </li>
+        <li>
+          <FaInstagram />
+        </li>
+        <li>
+          <FaLinkedin />
+        </li>
+      </ul>
+      <p>Nosso Rodapé</p>
+    </footer>
+  );
 }
 
 export default Footer;
